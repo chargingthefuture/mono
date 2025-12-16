84ca445f789c31053e1767c39318642e15834ae9
Farah <five-batboy-masses@duck.com>
2025-12-16 18:25:32 -0500

    refactor chyme app

--- chyme-android/build.gradle.kts
+++ chyme-android/build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file
 plugins {
     id("com.android.application") version "8.6.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
 task<Delete>("clean") {
