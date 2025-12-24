004aa7549b16177bbb31bfaeef82fe6c8e180117
Farah <five-batboy-masses@duck.com>
2025-12-24 11:39:51 -0500

    fix chyme build

--- chyme-android/app/build.gradle.kts
+++ chyme-android/app/build.gradle.kts
@@ -108,6 +108,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.compose.ui:ui:1.5.4")
     implementation("androidx.compose.material:material:1.5.4")
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
     implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.compose.ui:ui-tooling:1.5.4")
     implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
